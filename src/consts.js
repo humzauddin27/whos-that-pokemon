770bd9b054009a4fca06264f804a03652c5d5009
humzafaheemuddin <hfaheemuddin@nuvalence.io>
2021-09-11 22:19:13 -0400

    updating cdn values for region select

--- src/consts.js
+++ src/consts.js
@@ -1,17 +1,17 @@
 const kanto =
-  "https://cdn.bulbagarden.net/upload/thumb/8/80/Red_EN_boxart.png/400px-Red_EN_boxart.png";
+  "https://cdn2.bulbagarden.net/upload/thumb/8/80/Red_EN_boxart.png/400px-Red_EN_boxart.png";
 const johto =
-  "https://cdn.bulbagarden.net/upload/thumb/6/60/Crystal_EN_boxart.png/400px-Crystal_EN_boxart.png";
+  "https://cdn2.bulbagarden.net/upload/thumb/6/60/Crystal_EN_boxart.png/400px-Crystal_EN_boxart.png";
 const hoenn =
-  "https://cdn.bulbagarden.net/upload/thumb/6/65/Emerald_EN_boxart.jpg/400px-Emerald_EN_boxart.jpg";
+  "https://cdn2.bulbagarden.net/upload/thumb/6/65/Emerald_EN_boxart.jpg/400px-Emerald_EN_boxart.jpg";
 const sinnoh =
-  "https://cdn.bulbagarden.net/upload/thumb/8/83/Platinum_EN_boxart.png/400px-Platinum_EN_boxart.png";
+  "https://cdn2.bulbagarden.net/upload/thumb/8/83/Platinum_EN_boxart.png/400px-Platinum_EN_boxart.png";
 const unova =
-  "https://cdn.bulbagarden.net/upload/thumb/d/dc/Black_EN_boxart.png/400px-Black_EN_boxart.png";
+  "https://cdn2.bulbagarden.net/upload/thumb/d/dc/Black_EN_boxart.png/400px-Black_EN_boxart.png";
 const kalos =
-  "https://cdn.bulbagarden.net/upload/thumb/2/27/X_EN_boxart.png/400px-X_EN_boxart.png";
+  "https://cdn2.bulbagarden.net/upload/thumb/2/27/X_EN_boxart.png/400px-X_EN_boxart.png";
 const alola =
-  "https://cdn.bulbagarden.net/upload/thumb/3/38/Moon_EN_boxart.png/400px-Moon_EN_boxart.png";
+  "https://cdn2.bulbagarden.net/upload/thumb/3/38/Moon_EN_boxart.png/400px-Moon_EN_boxart.png";
 
 export const REGIONS = {
   KANTO: { num: 151, art: kanto },
@@ -20,5 +20,5 @@ export const REGIONS = {
   SINNOH: { num: 493, art: sinnoh },
   UNOVA: { num: 649, art: unova },
   KALOS: { num: 721, art: kalos },
-  ALOLA: { num: 802, art: alola }
+  ALOLA: { num: 802, art: alola },
 };
